05886a61d1f66e5cd4328fedb24186bfc60e162a
Dee Reddy <deesus@pm.me>
2022-05-24 09:41:45 -0700

    Bug: get correct cosine similarity values; ensure loading/processing single image vs entire dataset both produce same embedding.
    - The cosine similarity values were incorrect due to `image_dataset_from_directory` generating different array values than PIL, load_data, cv2.
    - Created custom image loading and processing via cv2 rather than Keras' `image_dataset_from_directory`, Rescaling, and Resizing layers.
    - Refactored/removed superfluous methods.

--- notebooks/product_image_similarity.py
+++ notebooks/product_image_similarity.py
@@ -17,6 +17,7 @@
 # standard lib:
 import heapq
 import pathlib
+import glob
 
 # ML/data science libraries:
 import numpy as np
@@ -26,113 +27,149 @@ import pandas as pd
 
 # TenserFlow/Keras classes:
 from tensorflow.keras.applications import ResNet50V2
-from tensorflow.keras.utils import image_dataset_from_directory, load_img
-from tensorflow.keras import Model, Sequential
-from tensorflow.keras.layers import Rescaling, Resizing, Reshape
+from tensorflow.keras import Sequential
+from tensorflow.keras.layers import GlobalMaxPool2D
 
 # plotting and imaging:
+import cv2
 import matplotlib.pyplot as plt
-from PIL import Image
+
 
 # + [markdown] pycharm={"name": "#%% md\n"}
 # ## Load and Process Images:
 
 # + pycharm={"name": "#%%\n"}
-data_dir = pathlib.Path('../data/e-commerce-product-images')
+# n.b. we don't need to sort since we're not training the model:
+def get_file_paths():
+    """ Create generator of all image file paths.
 
-image_list = list(data_dir.glob('**/*.jpg'))
-image_count = len(image_list)
-print('image count:', image_count)
+    :return {generator}
+    """
 
-# + pycharm={"name": "#%%\n"}
-# create dataset of images:
+    # We could also have used `pathlib.Path().glob()`, but that returns a POSIX path rather than str:
+    return glob.iglob('../data/e-commerce-product-images/**/*.jpg', recursive=True)
 
-IMG_HEIGHT  = 224
-IMG_WIDTH   = 224
-BATCH_SIZE  = 1
-VAL_SPLIT   = 0.2
-SEED        = 7
-
-train_set = image_dataset_from_directory(
-    data_dir,
-    validation_split=VAL_SPLIT,
-    seed=SEED,
-    subset='training',
-    image_size=(IMG_HEIGHT, IMG_WIDTH),
-    batch_size=BATCH_SIZE
-)
-
-val_set = image_dataset_from_directory(
-    data_dir,
-    validation_split=VAL_SPLIT,
-    seed=SEED,
-    subset='validation',
-    image_size=(IMG_HEIGHT, IMG_WIDTH),
-    batch_size=BATCH_SIZE
-)
+file_paths = get_file_paths()
 
 # + pycharm={"name": "#%%\n"}
 # explore data -- i.e. images:
 
-# if we used PIL, we could do `PIL.Image.open(image_list[100])`:
-example_img = np.array(Image.open(image_list[0]))
+example_img = cv2.imread(next(file_paths))
 print(plt.imshow(example_img))
+# TODO: print shapes of images (different dimensions, some grayscale)
 
-# + pycharm={"name": "#%%\n"}
-# processed image:
-
-# for single images:
-resize_and_scale = Sequential([
-    Resizing(IMG_HEIGHT, IMG_WIDTH),
-    Rescaling(1.0/255)
-])
-
-# for image dataset from `image_dataset_from_directory` (which already resize images):
-scale = Sequential([
-    Rescaling(1.0/255)
-])
-
-plt.imshow(resize_and_scale(example_img))
-
-# + pycharm={"name": "#%%\n"}
-# currently the shape of images are (height, width, channels):
-print('current shape of image:', example_img.shape)
-
-# but we want to add an extra dimension for batch size, i.e. shape of (batch size, height, width, channels):
-image_batch = np.expand_dims(example_img, axis=0)
+# reset generator:
+file_paths = get_file_paths()
 
 # + [markdown] pycharm={"name": "#%% md\n"}
 # ## Load ResNet Model:
 
 # + pycharm={"name": "#%%\n"}
-resnet_model = ResNet50V2(weights='imagenet')
+# constants:
+IMG_HEIGHT  = 224
+IMG_WIDTH   = 224
+
+resnet_model = ResNet50V2(
+    include_top=False,
+    weights='imagenet',
+    input_shape=(IMG_HEIGHT, IMG_WIDTH, 3)
+)
+resnet_model.trainable = False
+
 resnet_model.summary()
 
 # + pycharm={"name": "#%%\n"}
-# Since we're getting the "prediction" layer (last layer of ResNet), the results of the both models will be exactly the same:
-extracted_feature = Model(
-    name='ResNet50V2_ExtractFeature',
-    inputs=resnet_model.input,
-    outputs=resnet_model.get_layer('predictions').output
-)
+# Update the model by adding a global max pooling layer:
+# In essence, we replaced the last couple layers of the original ResNet model with a layer that outputs the embedding/features of the image.
+# Recall that global pooling always reduces the output to be shape 1 x 1 x channels; essentially, outputting a layer of feature-maps.
+model = Sequential([
+    resnet_model,
+    GlobalMaxPool2D()
+])
+
+model.summary()
+
+
+# + [markdown] pycharm={"name": "#%% md\n"}
+# **Aside:** if we didn't specify `include_top=False` in `resnet_model` from the previous two cells,
+# we could alternatively have created a new model and get the last 3rd to last layer:
+# ```
+# resnet_model_ = ResNet50V2(weights='imagenet')
+# ```
+#
+# This would, however, result in the exact same model:
+# ```
+# feature_extraction_model = Model(
+#     name='ResNet50V2_ExtractFeature',
+#     inputs=resnet_model_.inputs,
+#     outputs=resnet_model_.get_layer('post_relu').output
+# )
+# ```
+
+# + pycharm={"name": "#%%\n"}
+# Helper methods:
+
+def cosine_similarity(embedding_1:np.ndarray, embedding_2:np.ndarray):
+    """ Calculates the cosine similarity of two vectors.
+
+    :param embedding_1: An embedding vector.
+    :param embedding_2: An embedding vector.
+    :return: A single number (numpy float) -- the cosine similarity value.
+    """
+
+    # `distance.cosine` computes distance, not similarity; subtract by 1 to get similarity:
+    return 1 - spatial.distance.cosine(embedding_1, embedding_2)
+
+def process_image(img:np.ndarray):
+    """ Pre-process images before feeding to model.
+
+    Resizes image, scales (/255), and expands array dimension. The model requires specific input dimensions (shape),
+    therefore resizing and adding dimension is necessary. Scaling improves performance.
+    TODO: ideally, we should use standard deviation and mean of dataset instead of simply dividing by 255.
+
+    :param img:
+    :return {np.ndarray}: returns processed image
+    """
+    # see comparison of different interpolation methods: <https://chadrick-kwag.net/cv2-resize-interpolation-methods/>
+    processed_img = cv2.resize(img, (IMG_HEIGHT, IMG_WIDTH), interpolation=cv2.INTER_CUBIC)
+
+    # We can use TF's ResNet `preprocess_input` <https://www.tensorflow.org/api_docs/python/tf/keras/applications/resnet_v2/preprocess_input>
+    # however, because the datasets are quite different, it might do more harm than good <https://stackoverflow.com/a/58250681>;
+    # we can therefore, simply divide by 255:
+    processed_img = processed_img / 255
+
+    # ResNet model expects input shape (batch_size, img_height, img_width, channels) -- we need to add batch_size dimension:
+    processed_img = np.expand_dims(processed_img, axis=0)
+    return processed_img
+
+def get_embedding(file_path:str):
+    img = cv2.imread(file_path)
+    img = process_image(img)
+    embedding = model.predict(img)
+
+    return embedding
+
 
 # + pycharm={"name": "#%%\n"}
 # %%time
 
-# n.b. if you set shuffle to True for the dataset, then using `file_paths` (a fixed list) won't work:
-file_paths = train_set.file_paths
 df_embeddings = pd.DataFrame(columns=['file', 'file_path', 'embedding'])
 
-for i, (batch, _) in enumerate(train_set):
-    # # see <https://stackoverflow.com/a/63734183> on how to plot image from `image_dataset_from_directory`:
-    # print(plt.imshow(np.squeeze(batch.numpy().astype("uint8"))))
+# TODO: can we optimize/speed-up this, perhaps via batch processing?
+# TODO: replace POSIX file path with normal file path?
+for file_path in file_paths:
+    # TODO: replace with `get_embedding` method:
+    img = cv2.imread(file_path)
+    img = process_image(img)
 
-    processed = scale(batch)
-    # if we wanted to get the embedding/feature from a single image, we'd have to use `np.expand_dims`
-    # since we are processing a batch, the input shape is the expected (batch-size, height, width, channels):
-    embedding = extracted_feature.predict(processed)
+    embedding = model.predict(img)
+    file_name = file_path.split('/')[-1]
 
-    df_embeddings = df_embeddings.append({'file': file_paths[i].split('/')[-1], 'file_path': file_paths[i], 'embedding': embedding}, ignore_index=True)
+    df_embeddings = df_embeddings.append({'file': file_name, 'file_path': file_path, 'embedding': embedding}, ignore_index=True)
+
+# + pycharm={"name": "#%%\n"}
+# write out to file:
+df_embeddings.to_csv(pathlib.Path('../data/output/embedding.csv'))
 
 df_embeddings.head()
 
@@ -140,115 +177,56 @@ df_embeddings.head()
 # %%time
 
 n_images = len(df_embeddings)
-similarity = np.zeros((n_images, n_images))
+similarity_scores = np.zeros((n_images, n_images))
 for i in range(n_images):
     for j in range(n_images):
-        # `distance.cosine` computes distance, not similarity; subtract by 1 to get similarity:
-        similarity[i, j] = 1 - spatial.distance.cosine(df_embeddings.iloc[i]['embedding'], df_embeddings.iloc[j]['embedding'])
+        similarity_scores[i, j] = cosine_similarity(df_embeddings.iloc[i]['embedding'], df_embeddings.iloc[j]['embedding'])
 
 # + pycharm={"name": "#%%\n"}
 # create empty dataframe with file names as both the column and index names:
 file_names = df_embeddings.loc[:, 'file'].tolist()
-df_similarity = pd.DataFrame(similarity, columns=file_names, index=file_names)
+df_similarity = pd.DataFrame(similarity_scores, columns=file_names, index=file_names)
+
+# write out to file:
+df_similarity.to_csv(pathlib.Path('../data/output/similarity_scores.csv'))
+
 df_similarity.head()
 
 
 # + pycharm={"name": "#%%\n"}
-def find_most_similar_imgs(img_path:str, num_results:int=5):
-    # load single image, process, and get embedding:
-    target_img = np.array(Image.open(img_path))
-    target_img_processed = resize_and_scale(target_img)
-    target_embedding = extracted_feature.predict(np.expand_dims(target_img_processed, axis=0))
+def find_most_similar_images(img_path:str, num_results:int=5):
+    # Load single image, process, and get embedding:
+    target_embedding = get_embedding(img_path)
 
-    # find max embedding:    
+    # Find max embedding:
     top_matches = []
     for db_img_file_path, db_img_embedding in zip(df_embeddings['file_path'], df_embeddings['embedding']):
-        similarity = 1 - spatial.distance.cosine(target_embedding, db_img_embedding)
+        similarity_score = cosine_similarity(target_embedding, db_img_embedding)
 
-        # ensure heap has N number of elements (this is done by adding the first N items):
+        # TODO: top_results needs to exclude the target image itself from being returned
+
+        # We use `heapq` and keep only N number of elements -- this prevents us from holding the entire dataset in memory:
+        # Ensure heap has N number of elements (this is done by adding the first N items):
         if len(top_matches) < num_results:
-            heapq.heappush(top_matches, (similarity, db_img_file_path))
-        # after creating an N-element heap, if a new item has a LARGER value than the SMALLEST in the heap,
+            heapq.heappush(top_matches, (similarity_score, db_img_file_path))
+        # After creating an N-element heap, if a new item has a LARGER value than the SMALLEST in the heap,
         # then replace the smallest with the new item:
-        elif similarity > heapq.nsmallest(1, top_matches)[0][0]: # `heapq.nsmallest` returns a list, each element in list is a tuple (similarity, file_path); hence the reason for the double subscript `[0][0]`
-            heapq.heapreplace(top_matches, (similarity, db_img_file_path))
+        # `heapq.nsmallest` returns a list, each element in list is a tuple (similarity, file_path); hence the reason for the double subscript `[0][0]`:
+        elif similarity_score > heapq.nsmallest(1, top_matches)[0][0]:
+            heapq.heapreplace(top_matches, (similarity_score, db_img_file_path))
 
     return top_matches
 
-top_results = find_most_similar_imgs('../data/e-commerce-product-images/Footwear/Men/Images/images_with_product_ids/1637.jpg')
-
 
 # + pycharm={"name": "#%%\n"}
-def display_similar_imgs(img_path:str, num_results:int=5):
-    top_results = find_most_similar_imgs(img_path)
+def display_similar_images(img_path:str, num_results:int=5):
+    top_results = find_most_similar_images(img_path, num_results)
 
     # display multiple images; see <https://stackoverflow.com/q/19471814>:
     for similarity_score, file_path in top_results: # recall `top_results` returns a tuple of (similarity, file_path)
-        img = Image.open(file_path)
+        img = cv2.imread(file_path)
         plt.figure()
         plt.title(file_path.split('/')[-1]) # use file name as figure title
         plt.imshow(img)
 
-
-# + pycharm={"name": "#%%\n"}
-# example_img = '../data/e-commerce-product-images/Footwear/Men/Images/images_with_product_ids/1637.jpg'
-example_img = '../data/e-commerce-product-images/Apparel/Boys/Images/images_with_product_ids/4201.jpg'
-
-print('==================== original image: ====================')
-plt.figure()
-plt.title('4201.jpg')
-plt.imshow(Image.open(example_img))
-
-# + pycharm={"name": "#%%\n"}
-print('==================== similar images: ====================')
-display_similar_imgs(example_img)
-
-
-# + pycharm={"name": "#%%\n"}
-def similar_imgs(img1_path:str, img2_path:str):
-    img1 = np.array(Image.open(img1_path))
-    img2 = np.array(Image.open(img2_path))
-
-    img1 = resize_and_scale(img1)
-    img2 = resize_and_scale(img2)
-
-    img1_embedding = extracted_feature.predict(np.expand_dims(img1, axis=0))
-    img1_embedding = extracted_feature.predict(np.expand_dims(img2, axis=0))
-
-    similarity = 1 - spatial.distance.cosine(img1_embedding, img1_embedding)
-
-    return similarity
-
-
-def similarity_values(img1_path:str, img2_path:str):
-    img1_name = img1_path.split('/')[-1]
-    img2_name = img2_path.split('/')[-1]
-
-    results = df_similarity.loc['50174.jpg'].to_numpy()
-    return results[results == 1]
-
-
-# + pycharm={"name": "#%%\n"}
-similarity_values(
-    '../data/e-commerce-product-images/Apparel/Boys/Images/images_with_product_ids/4201.jpg',
-    '../data/e-commerce-product-images/Footwear/Women/Images/images_with_product_ids/41669.jpg'
-)
-
-# + pycharm={"name": "#%%\n"}
-df_similarity.head()
-
-# + pycharm={"name": "#%%\n"}
-plt.figure()
-plt.imshow(Image.open('../data/e-commerce-product-images/Apparel/Boys/Images/images_with_product_ids/4201.jpg'))
-plt.figure()
-plt.imshow(Image.open('../data/e-commerce-product-images/Footwear/Women/Images/images_with_product_ids/41669.jpg'))
-
-print('similarity:',
-      similar_imgs(
-        '../data/e-commerce-product-images/Apparel/Boys/Images/images_with_product_ids/4201.jpg',
-        '../data/e-commerce-product-images/Footwear/Women/Images/images_with_product_ids/41669.jpg'
-      )
-)
-
-# + pycharm={"name": "#%%\n"}
-
+display_similar_images('../data/e-commerce-product-images/Footwear/Men/Images/images_with_product_ids/20895.jpg')
